fa5e43e9a26b3ac61c6d67888eeb94fe71cdea89
mrfusion9k <49573900+mrfusion9k@users.noreply.github.com>
2021-09-05 13:17:12 -0700

    Battery script 1.0
    This script will keep track of windows battery state and shut off when the battery hits 5%.  This is for battery conditioning.  Typically if you replace a battery on a laptop you'll have to recondition it by draining to 5% and chargin back to 90%.  Instead of having to sit and check, this will automatically issue the shutdown command when it hits 5%.

---
+++ Battery_script.ps1
@@ -0,0 +1,17 @@
+﻿$logfilepath="C:\Users\brend\Documents\batterylog.txt"
+$condition = (Get-WmiObject win32_battery).estimatedChargeRemaining
+$logmessage="Starting battery script"
+Write-Host "Starting Battery Script"
+while ($Condition -ne 5)
+{
+    Write-Output $Condition
+    $logmessage=$Condition
+    $logmessage >> $logfilepath
+    Start-Sleep -s 30
+    $condition = (Get-WmiObject win32_battery).estimatedChargeRemaining
+    
+ }
+ Write-Host "Shutdown Computer @ 5% battery"
+ $logmessage="Shutdown Computer @ 5% battery"
+ $logmessage >> $logfilepath
+ Stop-Computer -ComputerName localhost
